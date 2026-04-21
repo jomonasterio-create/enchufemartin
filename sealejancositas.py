cd713a59e6d93c955efb302f768fad216a9d7327
jomonasterio-create <jomonasterio@duocuc.cl>
2026-04-21 16:53:38 -0400

    clasespollon.py

---
+++ sealejancositas.py
@@ -0,0 +1,27 @@
+# print("seleccione numeros para sumar")
+# asd=int(input("coloque un numero: "))
+
+# dsa=int(input("coloque otro numero "))
+
+# print(F"la suma de {asd} y {dsa} es:  {asd+dsa}")
+
+# clave=input("ingrese su clave")
+# if clave!="SHAZAM":
+#    print("clave incorrecta")
+# else:
+#    print("clave correcta")
+
+
+# name=input("cree su nombre de usuario de minimo 4 caracteres y max 10: ")
+# if len(name)<4:
+#    print("te faltan caracteres")
+# elif len(name)>10:
+#    print("te pasaste de caracteres")
+# else:
+#    print("nombre creado")
+
+pinn=(input("ingrese su pin: "))
+if len(str(pinn)) ==4:
+   print("entra")
+else:
+   print("no entra")
