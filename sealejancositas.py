3aece7758690addb03ede95f550e8237789dcf25
jomonasterio-crear <jo.monasterio@duocuc.cl>
2026-04-24 16:46:10 -0400

    mas tareas

--- sealejancositas.py
+++ sealejancositas.py
@@ -20,8 +20,29 @@
 # else:
 #    print("nombre creado")
 
-pinn=(input("ingrese su pin: "))
-if len(str(pinn)) ==4:
-   print("entra")
+# pinn=(input("ingrese su pin: "))
+# if len(str(pinn)) ==4:
+#    print("entra")
+# else:
+#    print("no entra")
+
+cand1=input("ingrese un candidato: ")
+cand2=input("ingrese otro candidato: ")
+v1=0
+v2=0
+votan=int(input("ingrese la cantidad de votantes: "))
+
+for i in range(votan):
+   voto=int(input(f"por quien votara?. 1.-{cand1} 2.-{cand2}"))
+   if voto==1:
+      v1+=1
+   elif voto==2:
+      v2+=1
+   else:
+      print("voto no valido")
+if v1>v2:
+   print(f"el ganador es {cand1} con {v1} votos") 
+elif:
+   print(f"el ganador es {cand2} con {v2} votos") 
 else:
-   print("no entra")
+   print("empate")  
